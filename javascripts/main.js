4f76c7eff1e5b0a5c87e16fb44f9ff2fbc24724a
Chris Sumption <fridausedge@gmail.com>
2016-05-19 21:22:08 -0700

    modify styling of index and video pages and add the poster image to the video / media page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(document).on('click',function(){
+	$('.collapse').collapse('hide');
+})
+
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
